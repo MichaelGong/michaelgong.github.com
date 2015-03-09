d0551821345fe05d5a6e11b7f4d6d533be033f62
HAO GONG <gonghao114@sina.com>
2015-03-09 17:05:52 +0800

    index.js
    首页的js文件

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+/**
+ * 首页的js
+ */
 var urlConfig = 'js/require.config.js';
 require([urlConfig],function(){
 	require(['loading'],function(M){
+		init(M);
+	});
+	
+	function init(M){
 		M.loading(1,1);
-	})
+	}
+
 });
+
