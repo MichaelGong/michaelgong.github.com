8018de9881ce8024a8fac6fa05d7f477fa38fbea
HAO GONG <gonghao114@sina.com>
2015-03-10 18:08:01 +0800

    index.js
    index.js

--- js/index.js
+++ js/index.js
@@ -3,8 +3,16 @@
  */
 var urlConfig = 'js/require.config.js';
 require([urlConfig],function(){
-	require(['loading','rainbow'],function(M,Rainbow){
-		Rainbow.color()
+	require(['loading','jquery','nicescroll','vector'],function(M,$,nicescroll,Vector){
+		$('html').niceScroll({cursorcolor:"#000",cursorborder:"none"});
+//		Rainbow.color()
+		var victor = new Victor('victor', 'output');
+		victor(["#35ac03", "#3f4303"]).set();
+		$('#hue-auto').on('mousemove',function(evt){
+			var procentjex = Math.round((evt.screenX / $(this).width())*255);
+      		var procentjey = ((evt.screenY / $(this).height())/2)+1;
+      		$('#hue-mouse').css('-webkit-filter', 'hue-rotate('+procentjex+'deg) contrast('+procentjey+')');
+		});
 	});
 	
 	
